31bde67b211e0ca82ecd2425409dc13564d1c73e
andysengupta <13270494+andysengupta@users.noreply.github.com>
2025-10-04 14:31:44 +0200

    Fix: add Combine imports for RideStore and LocationManager to satisfy ObservableObject conformance.

--- Kinetiq Drive/Kinetiq Drive/LocationManager.swift
+++ Kinetiq Drive/Kinetiq Drive/LocationManager.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreLocation
+import Combine
 
 final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published private(set) var authorizationStatus: CLAuthorizationStatus = .notDetermined
--- Kinetiq Drive/Kinetiq Drive/Models.swift
+++ Kinetiq Drive/Kinetiq Drive/Models.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreLocation
+import Combine
 
 struct Ride: Identifiable, Codable {
     let id: UUID
